b9670b4ffde074db3b6c2155b413ad41e3ead286
hope-11 <hope-11@163.com>
2017-07-19 18:08:48 +0800

    study session and use

---
+++ client/helpers/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by houpeng on 2017/7/19.
+ */
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+});
---
+++ client/templates/application/layout.js
@@ -0,0 +1,8 @@
+/**
+ * Created by houpeng on 2017/7/19.
+ */
+Template.layout.helpers({
+    pageTitle: function(){
+        return Session.get('pageTitle');
+    }
+})
