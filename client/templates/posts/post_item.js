539e57b32d18e5ea215144451be4614bdd9618cc
hope-11 <hope-11@163.com>
2017-07-22 18:11:26 +0800

    edit post

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,11 @@
  * Created by houpeng on 2017/7/15.
  */
 Template.postItem.helpers({
+
+    ownPost: function(){
+        return this.userId === Meteor.userId();
+    },
+
     domain: function(){
         var a=document.createElement('a');
         a.href=this.url;
