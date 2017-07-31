3bceca6ecc6399c32ac1d71cf4ec37fda905a69d
hope-11 <hope-11@163.com>
2017-07-31 18:14:57 +0800

    study animate

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -5,9 +5,9 @@ Template.header.helpers({
     activeRouteClass: function () {
         var args=Array.prototype.slice.call(arguments, 0);
         args.pop();
-console.log(args)
+
         var active=_.any(args, function (name) {
-            console.log("name--"+name)
+
             return Router.current() && Router.current().route.getName() === name;
         });
 
